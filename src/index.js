c0fd7acc2fd3d7d33cde0e18fc423f79d23f9107
Asad Nisar <asadnisar98@gmail.com>
2024-02-06 13:51:57 +0400

    initial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom"; // Correct import statement for ReactDOM
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
