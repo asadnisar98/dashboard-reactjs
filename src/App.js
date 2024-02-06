95fbd97be6ac6ed9670ab320a132604582e8f9b6
Asad Nisar <asadnisar98@gmail.com>
2024-02-06 14:53:42 +0400

    pages / components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const activeMenu = false;
 
 
   return (
@@ -33,11 +33,45 @@ const App = () => {
           )}
 
               <div className={
-                activeMenu ? 'dark:bg-main-bgbg-main-bg min-h-screen md:ml-72 w-full'
-                : 'dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2'
+                `dark:bg-main-bgbg-main-bg min-h-screen w-full
+                ${activeMenu ? 'md:ml-72' : 'w-full flex-2' }`
               }>
+                <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                    Navbar
+                </div>
 
               </div> 
+
+              <div>
+                <Routes>
+                {/* dahboard */}
+                  <Route path='/' element="ecomerce" />
+                  <Route path='/ecommerce' element="Ecomerce" />
+
+                {/* pages */}
+
+                  <Route path='/orders' element="orders" />
+                  <Route path='/employees' element="employees" />
+                  <Route path='/customers' element="customers" />
+
+                  {/* Apps */}
+
+                  <Route path='/kanban' element="kanban" />
+                  <Route path='/editor' element="editor" />
+                  <Route path='/calendor' element="calendor" />
+                  <Route path='/color-pikcer' element="color picker" />
+
+                  {/* charts */}
+                  <Route path='/line' element="line" />
+                  <Route path='/area' element="area" />
+                  <Route path='/bar' element="bar" />
+                  <Route path='/pie' element="pie" />
+                  <Route path='/financial' element="financial" />
+                  <Route path='/color-mapping' element="color-mapping" />
+                  <Route path='/pyramind' element="pyramind" />
+                  <Route path='/stacked' element="stacked" />
+                </Routes>
+              </div>
 
         </div>
       </BrowserRouter>
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const buttton = () => {
+  return (
+    <div>buttton</div>
+  )
+}
+
+export default buttton
